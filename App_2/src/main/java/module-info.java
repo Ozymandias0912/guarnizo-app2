b6a10fe682bcad9b5f643d7fca4a7e23dc200706
Juan <jdavid29a@gmail.com>
2021-11-10 14:39:45 -0500

    initial GUI design

---
+++ App_2/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.app_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.app_2 to javafx.fxml;
+    exports com.example.app_2;
+    exports baseline;
+    opens baseline to javafx.fxml;
+}
